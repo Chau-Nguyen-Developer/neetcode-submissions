c1142a13cdb8764a4a160c5b99349ecb6fb5af91
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2026-08-02 22:41:39 -0500

    Add: buy-and-sell-crypto - submission-11

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-11.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int minBuy = prices[0];
+        int maxP = 0;
+        for (int sell: prices)
+        {
+            maxP = max(maxP, sell - minBuy);
+            if(sell < minBuy)
+            {
+                minBuy = sell;
+            }
+        }
+
+        return maxP;
+    }
+};
